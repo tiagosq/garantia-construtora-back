373832da1ce63b629f41a6a1e6a26f6265836650
Wesley Matos <wamk-9@hotmail.com>
2024-06-17 23:58:01 -0300

    Updated JSON returns on auth controller and role seeder.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -41,7 +41,7 @@ class AuthController extends Controller
     {
         $credentials = request(['email', 'password']);
         if (!$token = auth()->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->json(['message' => 'Unauthorized'], 401);
         }
 
         return $this->respondWithToken($token);
@@ -67,9 +67,12 @@ class AuthController extends Controller
     protected function respondWithToken($token)
     {
         return response()->json([
-            'access_token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
+            'data' =>
+            [
+                'access_token' => $token,
+                'token_type' => 'bearer',
+                'expires_in' => auth()->factory()->getTTL() * 60
+            ]
         ]);
     }
 }
--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 use Symfony\Component\Uid\Ulid;
 
@@ -13,18 +14,173 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('roles')->insert([
-            'id' => Ulid::generate(),
-            'name' => 'Administrador',
-            'permissions' => '{}',
-            'status' => true,
-        ]);
+        // ---------------------- Individual Permissions ----------------------
+        $attachmentPermissions = [
+            'upload',
+            'download',
+        ];
 
-        DB::table('roles')->insert([
-            'id' => Ulid::generate(),
-            'name' => 'Usuário',
-            'permissions' => '{}',
-            'status' => true,
-        ]);
+        $buildingPermissions = [
+            'create',
+            'read',
+        ];
+
+        $businessPermissions = [
+            'create',
+            'read',
+        ];
+
+        $logPermissions = [
+            'read',
+        ];
+
+        $maintenancePermissions = [
+            'create',
+            'read',
+        ];
+
+        $questionPermissions = [
+            'create',
+            'read',
+            'reply',
+        ];
+
+        $rolePermissions = [
+            'create',
+        ];
+
+        $userPermissions = [
+            'create',
+        ];
+
+        // ----------------------- Permissions by Role ------------------------
+        $userRole = [];
+        $managerRole = [];
+        $adminRole = [];
+        $superAdminRole = [];
+
+        array_walk($attachmentPermissions, function($key) use (&$superAdminRole, &$adminRole, &$managerRole, &$userRole)
+        {
+            $superAdminRole['attachment'][$key]   = true;
+            $adminRole['attachment'][$key]        = true;
+            $managerRole['attachment'][$key]      = true;
+            $userRole['attachment'][$key]         = true;
+        });
+
+        array_walk($buildingPermissions, function($key) use (&$superAdminRole, &$adminRole, &$managerRole, &$userRole)
+        {
+            $superAdminRole['building'][$key]   = true;
+            $adminRole['building'][$key]        = true;
+            $managerRole['building'][$key]      = true;
+            $userRole['building'][$key]         = false;
+        });
+
+        array_walk($businessPermissions, function($key) use (&$superAdminRole, &$adminRole, &$managerRole, &$userRole)
+        {
+            $superAdminRole['business'][$key]   = true;
+            $adminRole['business'][$key]        = false;
+            $managerRole['business'][$key]      = false;
+            $userRole['business'][$key]         = false;
+        });
+
+
+        array_walk($logPermissions, function($key) use (&$superAdminRole, &$adminRole, &$managerRole, &$userRole)
+        {
+            $superAdminRole['log'][$key]   = true;
+            $adminRole['log'][$key]        = true;
+            $managerRole['log'][$key]      = false;
+            $userRole['log'][$key]         = false;
+        });
+
+        array_walk($maintenancePermissions, function($key) use (&$superAdminRole, &$adminRole, &$managerRole, &$userRole)
+        {
+            $superAdminRole['maintenance'][$key]   = true;
+            $adminRole['maintenance'][$key]        = true;
+            $managerRole['maintenance'][$key]      = true;
+            $userRole['maintenance'][$key]         = false;
+        });
+
+        array_walk($questionPermissions, function($key) use (&$superAdminRole, &$adminRole, &$managerRole, &$userRole)
+        {
+            $superAdminRole['question'][$key]   = true;
+            $adminRole['question'][$key]        = true;
+            $managerRole['question'][$key]      = true;
+            $userRole['question'][$key]         = (in_array($key, ['create']) ? false : true);
+        });
+
+
+        array_walk($rolePermissions, function($key) use (&$superAdminRole, &$adminRole, &$managerRole, &$userRole)
+        {
+            $superAdminRole['role'][$key]   = true;
+            $adminRole['role'][$key]        = false;
+            $managerRole['role'][$key]      = false;
+            $userRole['role'][$key]         = false;
+        });
+
+        array_walk($userPermissions, function($key) use (&$superAdminRole, &$adminRole, &$managerRole, &$userRole)
+        {
+            $superAdminRole['user'][$key]   = true;
+            $adminRole['user'][$key]        = true;
+            $managerRole['user'][$key]      = false;
+            $userRole['user'][$key]         = false;
+        });
+
+        // ----- Database inserts/updates of default roles of this seeder -----
+        DB::table('roles')->updateOrInsert(
+            [
+                'name' => 'SuperAdmin',
+            ],
+            [
+                'id' => Ulid::generate(),
+                'name' => 'SuperAdmin',
+                'permissions' => $superAdminRole,
+                'status' => true,
+                'created_at' => Date::now(),
+                'updated_at' => Date::now(),
+            ]
+        );
+
+        DB::table('roles')->updateOrInsert(
+            [
+                'name' => 'Admin',
+            ],
+            [
+                'id' => Ulid::generate(),
+                'name' => 'Admin',
+                'permissions' => $adminRole,
+                'status' => true,
+                'created_at' => Date::now(),
+                'updated_at' => Date::now(),
+
+            ]
+        );
+
+        DB::table('roles')->updateOrInsert(
+            [
+                'name' => 'Gerente',
+            ],
+            [
+                'id' => Ulid::generate(),
+                'name' => 'Gerente',
+                'permissions' => $managerRole,
+                'status' => true,
+                'created_at' => Date::now(),
+                'updated_at' => Date::now(),
+            ]
+        );
+
+        DB::table('roles')->updateOrInsert(
+            [
+                'name' => 'Usuário',
+            ],
+            [
+                'id' => Ulid::generate(),
+                'name' => 'Usuário',
+                'permissions' => $userRole,
+                'status' => true,
+                'created_at' => Date::now(),
+                'updated_at' => Date::now(),
+            ]
+        );
     }
 }
